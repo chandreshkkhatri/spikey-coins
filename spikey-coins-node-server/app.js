153cf55af88ff547e3e40a7017e9f86b7790ba30
Chandresh Kumar <chandresh.code@gmail.com>
2024-07-11 15:27:36 +0530

    fix: remove bittrex and polonix routes

--- spikey-coins-node-server/app.js
+++ spikey-coins-node-server/app.js
@@ -4,13 +4,9 @@ const port = 8000
 const app = express()
 
 const binanceRouter = require('./routers/binance-router')
-// const poloniexRouter = require('./routers/poloniex-router')
-// const bittrexRouter = require('./routers/bittrex-router')
 
 app.use(cors())
 app.use('/binance', binanceRouter)
-// app.use('/poloniex', poloniexRouter)
-// app.use('/bittrex', bittrexRouter)
 
 app.get('/', (req, res) => {
     res.send(`Hi, you've reached the proxy server. We tunnel your requests to byass cors issues`)
