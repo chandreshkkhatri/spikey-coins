92eeb2c65ff5f89ab8847ac13c452155d8bba51c
Chandresh Kumar <chandresh.code@gmail.com>
2025-06-06 03:35:57 +0530

    updates baseurl source from environment variable

--- react-ui/src/utils/api.js
+++ react-ui/src/utils/api.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import config from "./config.json";
 
 // API configuration
-const API_BASE_URL = config.base_url;
+const API_BASE_URL =
+  process.env.REACT_APP_API_BASE_URL || "http://localhost:8000";
 
 // Create axios instance with default config
 const apiClient = axios.create({
