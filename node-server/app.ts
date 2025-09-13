7cd12ec503f209c493d1d0bce73f6b10eae27239
Chandresh Kumar <chandresh.code@gmail.com>
2025-09-13 23:15:04 +0530

    add dotenv configuration

--- node-server/app.ts
+++ node-server/app.ts
@@ -2,6 +2,9 @@
  * Spikey Coins Crypto Data Server
  * Real-time cryptocurrency data proxy with WebSocket streams
  */
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
